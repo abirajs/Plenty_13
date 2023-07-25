047d166d9f94bd9482bf4ea5f376d1ae6d81f24a
abirajs <novalnetchennai@gmail.com>
2023-07-25 11:50:52 +0530

    Add files via upload

---
+++ src/Constants/NovalnetConstants.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is used to define the constants
+ *
+ * @author       Novalnet AG
+ * @copyright(C) Novalnet
+ * @license      https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ */
+namespace Novalnet\Constants;
+
+/**
+ * Class NovalnetConstants
+ *
+ * @package Novalnet\Constants
+ */
+class NovalnetConstants
+{
+    const PLUGIN_VERSION                    = '7.0.0-NN(12.0.2)';
+    const MERCHANT_DETAILS                  = 'https://payport.novalnet.de/v2/merchant/details';
+}
